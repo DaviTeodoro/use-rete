ac2922dbba4c4ceb4f0a93407ae6a1cea0296f34
Davi Teodoro <davi.a.teodoro@gmail.com>
2022-03-15 19:08:12 -0300

    export retesocket

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ export { default as NodeEditor } from './components/NodeEditor';
 export { default as Node } from './components/Node';
 export { default as Socket } from './components/Socket';
 export { ReteProvider, useRete } from './provider';
+import Rete from 'rete';
+const ReteSocket = Rete.Socket;
+export { ReteSocket };
